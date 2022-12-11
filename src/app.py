0b6c2caaa98cb5e0816203526169f2e2baea9196
x06lan <x065p4mp6@gmail.com>
2022-12-11 15:25:09 +0800

    base web function is done but maybe the ncat have error

--- src/app.py
+++ src/app.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 @app.route("/",methods=["GET"])
 def main():
     tem="404"
-    with open ('../templates/chat.html', 'r') as file:
+    with open ('./templates/chat.html', 'r') as file:
         tem=file.read()
     return tem 
 @app.route("/hello")
 def hello():
-    return render_template('../templates/hello.html')
+    return render_template('./templates/hello.html')
 
 @app.route('/chat', methods=[ 'GET',"POST"])
 def chatPage():
@@ -22,7 +22,7 @@ def chatPage():
         with open ('testChat.txt', 'a') as file:
             file.writelines(user)
     
-    with open ('../templates/chat.html', 'r') as file:
+    with open ('./templates/chat.html', 'r') as file:
         tem=file.read()
     return tem 
     # return render_template('')
@@ -30,16 +30,28 @@ def chatPage():
 @app.route('/post', methods=[ 'POST'])
 def pushInfo():
     print(request.data)
+    newMessage=json.loads(request.data)
+    print(newMessage["name"])
+    with open ('data.json', 'r+') as file:
+        data=file.read()
+        data=json.loads(data)
+        data.append(newMessage)
+        with open ('data.json', 'w') as Writefile:
+            Writefile.write(json.dumps(data))
+
+    # with open ('testChat.txt', 'a') as file:
+    #     file.writelines(data["name"]+":"+data["text"])
     return "ok"
+
 @app.route('/get', methods=[ 'GET'])
 def getInfo():
-    # data=""
-    data={"wtf":"hi"}
-    # with open ('data.json', 'a') as file:
-    #     data=file.read()
+    data=""
+    # data={"wtf":"hi"}
+    with open ('data.json', 'r+') as file:
+        data=file.read()
     response = app.response_class(
-        response=json.dumps(data),
-        # response=info,
+        # response=json.dumps(data),
+        response=data,
         status=200,
         mimetype='application/json'
     )
--- src/connectNcat.py
+++ src/connectNcat.py
@@ -12,11 +12,11 @@ def follow (thefile):
         yield line
 
 if __name__ == '__main__':
-    fileName="messge.txt"
+    fileName="1.txt"
     child = pexpect.spawnu('ncat 127.0.0.1 1234 -o '+fileName, logfile=sys.stdout)
-    test = pexpect.spawnu('python3 txt2json.py '+fileName+' messge.json')
-    test = pexpect.spawnu('python3 app.py')
-    with open("testChat.txt", "r") as logfile:
+    test = pexpect.spawnu('python3 txt2json.py '+fileName+' 1.json')
+    web = pexpect.spawnu('python3 ./src/app.py')
+    with open("./src/testChat.txt", "r") as logfile:
         loglines = follow(logfile)
         for line in loglines:
             child.sendline(line)
