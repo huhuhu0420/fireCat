0adde3460d2ba6b9c2741d156e2caac01bc926b0
huhuhu0420 <ghostcheng.00@gmail.com>
2022-12-13 11:25:35 +0800

    switch 1.json to tinydb format

--- src/txt2json.py
+++ src/txt2json.py
@@ -1,6 +1,7 @@
 import time
 import json
 import sys
+from tinydb import TinyDB
 
 def follow (thefile):
     thefile.seek(0, 2)
@@ -27,39 +28,41 @@ def analyze (line: str ,fileName:str) -> None:
     global selfCode
     attribute = str()
     user = str()
-    with open(fileName, "a") as outFile:
-        # self message
-        if (line[0] != '<'):
-            attribute = 'user'
-            global selfCode
-            user = selfCode
-            content = line[:-1]
-            outDict['content'] = content
-        # announce
-        elif (line[1] == 'a'):
-            attribute = 'announce'
-            if ("is connected" in line):
-                outDict["type"] = "newConnected"
-            elif ("already connected" in line):
-                outDict["type"] = "alreadyConnected"
-            elif ("is disconnected" in line):
-                outDict["type"] = "disconnected"
-            user = getUserCodeList(line)
-        # other user message
-        elif (line[1] == 'u'):
-            user = line[5]
-            attribute = 'user'
-            content = line[8:-1]
-            outDict['content'] = content
-        outDict['attribute'] = attribute
-        outDict['user'] = user 
-        json.dump(outDict, outFile, indent=2)
+    # self message
+    if (line[0] != '<'):
+        attribute = 'user'
+        global selfCode
+        user = selfCode
+        content = line[:-1]
+        outDict['content'] = content
+    # announce
+    elif (line[1] == 'a'):
+        attribute = 'announce'
+        if ("is connected" in line):
+            outDict["type"] = "newConnected"
+        elif ("already connected" in line):
+            outDict["type"] = "alreadyConnected"
+        elif ("is disconnected" in line):
+            outDict["type"] = "disconnected"
+        user = getUserCodeList(line)
+    # other user message
+    elif (line[1] == 'u'):
+        user = line[5]
+        attribute = 'user'
+        content = line[8:-1]
+        outDict['content'] = content
+    outDict['attribute'] = attribute
+    outDict['user'] = user 
+
+    db.insert(outDict)
 
 def clearFile (fileName: str) -> None:
     open(fileName, "w").close()
 
 selfCode = 0
 if __name__ == '__main__':
+    db = TinyDB('1.json')  # init db
+
     arg=sys.argv[1:]
     if(len(arg)<2):
         print("error with arg\n")
